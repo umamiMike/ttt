00824c2d80dc6e923b562b66a2f6b549d4fe190a
mike wilding <heymichaelwilding@gmail.com>
2025-09-14 12:19:32 -0700

    knocking up the web server with an experiment using svg as the ui design

--- web/server.py
+++ web/server.py
@@ -1,14 +1,34 @@
+import os
 import http.server
-import socketserver
 
-PORT = 8000
-
-
-class IndexHandler(http.server.SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, directory="static/", **kwargs)
+PORT = 8005
 
 
-with socketserver.TCPServer(("", PORT), IndexHandler) as httpd:
-    print(f"serving at port {PORT}")
+class StaticHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.path = "index.html"
+        return super().do_GET()
+
+class MainServer(http.server.BaseHTTPRequestHandler):
+
+    def _set_headers(self, code=200, content_type="application/json"):
+        self.send_response(code)
+        self.send_header("Content-type",content_type)
+        self.end_headers()
+
+    # def do_GET(self):
+    #     if self.path.startswith("/static/"):
+    #         self.path = self.path[len(".static/"):]
+    #         return super().do_GET()
+
+
+def run(port=PORT):
+    os.chdir("web_client")
+    httpd = http.server.HTTPServer(("", port), StaticHandler)
+    print(f"serving static from http://localhost:{port}/web_client/")
     httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
--- web/session.py
+++ web/session.py
@@ -1,1 +1,28 @@
+from dataclasses import dataclass, field
 from core.game import Game
+
+@dataclass
+class Player:
+    name: str
+    games_won: int = field(default=0)
+
+
+
+@dataclass
+class Session:
+    player_x: Player
+    player_y: Player
+
+    games_played: int = field(default=0)
+
+    def start(self):
+        game = Game()
+        # prompt the x player to take a turn
+        print(game.check_winner())
+
+    # a player connects, now we are waiting for another  player to connect to
+    # connects the players and a set of games they play
+
+if __name__ == "__main__":
+    pass
+
