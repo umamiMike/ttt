b4c4ffdcfe58b3e16906c15c99b77ffeb7eddb16
mike wilding <heymichaelwilding@gmail.com>
2025-09-15 21:01:08 -0700

    fleshing out messaging loop

--- broker/broker.py
+++ broker/broker.py
@@ -1,4 +1,6 @@
 import paho.mqtt.client as mqtt
+from web.session import Session
+import json
 
 BROKER = "localhost"
 TOPIC_MOVES = "game/move"
@@ -7,9 +9,16 @@ TOPIC_STATE = "game/state"
 def on_connect(client, userdata, flags, rc, properties=None):
     print("Connected with result code", rc)
     client.subscribe(TOPIC_STATE)
+    client.subscribe(TOPIC_MOVES)
 
 def on_message(client, userdata, msg):
-    print("State update:", msg.payload.decode())
+    message = json.loads(msg.payload.decode())
+    print(message)
+
+    if message["action"] == "connect":
+        breakpoint()
+    if message["action"] == "take_turn":
+        breakpoint()
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id="player_1",)
 client.on_connect = on_connect
---
+++ todo.md
@@ -0,0 +1,8 @@
+## web client
+
+
+### init
+
+- disallow clicking board
+- prompt user for name
+
