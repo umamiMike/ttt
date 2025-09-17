81d26b02bc5e02be545d09cf60076a0b28a4a156
mike wilding <heymichaelwilding@gmail.com>
2025-09-16 22:22:05 -0700

    frontend web work: peels off js app.js

---
+++ web_client/app.js
@@ -0,0 +1,112 @@
+
+		const client = mqtt.connect("ws://localhost:9001"); // Mosquitto WS listener
+
+		client.on("connect", () => {
+			console.log("Connected to broker!");
+			client.subscribe("game/state", (err) => {
+				if (!err) {
+					msg = {action: "browser_connect", player: "", data: ""}
+					client.publish("game/move", JSON.stringify(msg));
+				}
+			});
+		});
+
+		client.on("message", (topic, message) => {
+			console.log(topic, message.toString());
+		});
+
+		client.on("error", (topic, message) => {
+			console.log(topic, message.toString());
+		});
+
+		const boardElement = document.getElementById('board');
+		const statusElement = document.getElementById('status');
+		const player_x = document.getElementById('player_x');
+		const resetButton = document.getElementById('reset');
+		const query_button = document.getElementById('query');
+
+
+		let board = Array(9).fill('');
+		let bs = Array(9).fill(0);
+		let currentPlayer = 'X';
+		let gameOver = false;
+
+		function playerInfo() {
+			const name = "mike"
+			const name_prompt = document.createElement('input');
+			name_prompt.placeholder = "Player Name"
+			name_prompt.addEventListener("keypress", (e) => {
+				if (e.key === "Enter") {
+					const name = document.createElement('div')
+					name.textContent = name_prompt.value
+					const par = name_prompt.parentElement
+					par.appendChild(name)
+					// name.textContent = name_prompt.value
+					name_prompt.remove()
+					join_game(name.textContent)
+					makeClickable()
+				}
+			});
+			player_x.appendChild(name_prompt)
+
+		}
+		function join_game(name) {
+			msg = {action: "join", data: name}
+			client.publish("game/move", JSON.stringify(msg), {qos: 1});
+		}
+		function initBoard() {
+			boardElement.innerHTML = '';
+			board.forEach((cell, i) => {
+				const cellDiv = document.createElement('div');
+				cellDiv.classList.add('cell');
+				cellDiv.textContent = cell;
+				boardElement.appendChild(cellDiv);
+			});
+		}
+		function makeClickable() {
+			cells = document.querySelectorAll('.cell')
+			console.log(cells)
+			cells.forEach((cell, i) => {
+				cell.addEventListener('click', () => handleMove(i));
+			});
+		}
+
+		function update_board(cells) {
+			boardElement.innerHTML = '';
+			board.forEach((cell, i) => {
+				const cellDiv = document.createElement('div');
+				cellDiv.classList.add('cell');
+				cellDiv.textContent = cells[i];
+				boardElement.appendChild(cellDiv);
+			}
+			}
+
+		function handleMove(index) {
+			console.log(index)
+			const px = document.getElementById("player_x")
+			const name = px.textContent
+			msg = {action: "take_turn", data: {player: name, cell: index}}
+			client.publish("game/move", JSON.stringify(msg), {qos: 1});
+
+		}
+
+		function checkWin(player) {
+		}
+
+		resetButton.addEventListener('click', () => {
+			board = Array(9).fill('');
+			currentPlayer = 'X';
+			gameOver = false;
+			statusElement.textContent = `Next turn: ${currentPlayer}`;
+			renderBoard();
+		});
+
+		query_button.addEventListener('click', (e) => {
+			bn = board.map(item => {return item === "X" ? 1 : item === "O" ? -1 : 0})
+			client.publish("game/state", JSON.stringify(bn), {qos: 1});
+
+		});
+		// Initial render
+		initBoard();
+		playerInfo();
+		statusElement.textContent = `Next turn: ${currentPlayer}`;
